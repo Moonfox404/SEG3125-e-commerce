b8096abf113ba57f814e1ff78ec2add57043ed5b
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2025-07-04 14:02:46 -0400

    update product using params instead

---
+++ e-commerce/app/product/[product_id]/page.tsx
@@ -0,0 +1,29 @@
+import { MockProducts } from "@/app/mock-data/MockProducts";
+import NavBar from "../../components/NavBar";
+import ProductDetails from "../../components/ProductDetails";
+import ProductImages from "../../components/ProductImages";
+
+export default async function Product({
+  params,
+}: {
+  params: Promise<{ product_id: number }>;
+}) {
+  const { product_id } = await params;
+
+  const currentProduct = MockProducts[product_id];
+  return (
+    <div>
+      <NavBar itemsInCart={2} />
+      <div className="w-full h-screen flex items-center justify-center">
+        {currentProduct ? (
+          <>
+            <ProductImages productID={product_id} />
+            <ProductDetails productID={product_id} />
+          </>
+        ) : (
+          `Could not find Product with id ${product_id}. Please return to items page`
+        )}
+      </div>
+    </div>
+  );
+}
