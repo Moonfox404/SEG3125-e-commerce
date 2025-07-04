7dc7f5fba237216d7e5cbe461c5a226d8fce375d
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2025-07-04 01:08:35 -0400

    Set up Product Page and cart, add Product page components - ProductImages, ProductDetails

--- e-commerce/app/mock-data/MockProducts.ts
+++ e-commerce/app/mock-data/MockProducts.ts
@@ -6,22 +6,25 @@ export type Product = {
   numRatings: number;
   price: number;
   discountedPrice?: number;
-  styles?: string[];  // colours
+  styles?: string[]; // colours
   inStock: boolean;
   discounted: boolean;
+  images: number[]; // multiple product images
 };
 
 export const MockProducts: Product[] = [
   {
     id: "0",
     name: "Couch",
-    details: "For sitting lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquet, tortor ac facilisis vestibulum, nisl leo malesuada libero, quis facilisis sem est quis ipsum.",
-    rating: 4.3,
+    details:
+      "For sitting lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquet, tortor ac facilisis vestibulum, nisl leo malesuada libero, quis facilisis sem est quis ipsum.",
+    rating: 4.5,
     numRatings: 207,
     price: 5075.95,
     discountedPrice: 3975.97,
     styles: ["red", "green", "blue", "purple"],
     inStock: true,
-    discounted: true
-  }
-]
+    discounted: true,
+    images: [1, 2, 3, 4, 5],
+  },
+];
