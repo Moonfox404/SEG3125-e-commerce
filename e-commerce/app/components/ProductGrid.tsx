a722d00df22efc521e3b6305308c0c3b20dbde14
Xuan Pham <hpham024@uottawa.ca>
2025-07-05 23:39:49 -0400

    add no items message

--- e-commerce/app/components/ProductGrid.tsx
+++ e-commerce/app/components/ProductGrid.tsx
@@ -14,29 +14,39 @@ const ProductGrid = ({
 
   return (
     <div className="size-full">
-      <div className="flex items-center w-full">
-        <p className="w-full text-center">{9 * (page - 1) + 1} - {Math.min(9 * page, products.length)} of {products.length} items</p>
-      </div>
+      {
+        products.length > 0 ?
+          <>
+            <div className="flex items-center w-full">
+              <p className="w-full text-center">{9 * (page - 1) + 1} - {Math.min(9 * page, products.length)} of {products.length} items</p>
+            </div>
 
-      {/* items */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {
-          products.slice(9 * (page - 1), Math.min(9 * page, products.length)).map((product, idx) => {
-            return (
-              <div key={idx} className="col">
-                <ProductCard product={product} />
+            {/* items */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              {
+                products.slice(9 * (page - 1), Math.min(9 * page, products.length)).map((product, idx) => {
+                  return (
+                    <div key={idx} className="col">
+                      <ProductCard product={product} />
+                    </div>
+                  );
+                })
+              }
+            </div>
+
+            {/* pagination */}
+            <div className="flex justify-center items-center">
+              <div className="w-fit">
+                <Pagination numPages={Math.ceil(products.length / 9)} onToggle={(pg) => { setPage(pg); }} />
               </div>
-            );
-          })
-        }
-      </div>
-
-      {/* pagination */}
-      <div className="flex justify-center items-center">
-        <div className="w-fit">
-          <Pagination numPages={Math.ceil(products.length / 9)} onToggle={(pg) => { setPage(pg); }} />
-        </div>
-      </div>
+            </div>
+          </>
+          :
+          <div className="text-center">
+            <p className="text-3xl my-5">Oops... No Items Found</p>
+            <p className="text-lg">Consider making your search more broad or removing some filters.</p>
+          </div>
+      }
     </div>
   );
 };
